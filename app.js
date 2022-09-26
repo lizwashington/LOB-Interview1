25b24426cf6a7e87323413134ebbc98561fb678a
Liz Washington <101292397+lizwashington@users.noreply.github.com>
2022-09-25 20:33:26 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import React from 'react';
+// ...
+import { BrowserRouter } from 'react-router-dom';
 
-const addressRouter = require('./routes/address')
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/address', addressRouter);
-
-module.exports = app;
+ReactDOM.render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+  , document.getElementById('root')
+);
+// ...
